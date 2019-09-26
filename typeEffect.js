eb16f13c4eb8247fd114c8951d5937df74125948
uie-823 <yuenwu1996@gms.tku.edu.tw>
2019-09-26 19:21:52 +0800

    打字特效

--- typeEffect.js
+++ typeEffect.js
@@ -1,15 +1,30 @@
-var texts=["平面設計師","攝影師","插畫工作者"]    // 陣列
-var speed=1000;                                //打字速度
-var index=0;                                   //編號
-var count=0;                                   //字數
-
-// // 測試
-// document.getElementById("type-box").innerHTML=texts[0];
+var texts = ["平面設計師", "插畫工作者", "攝影師"];   // 陣列
+var speed = 1000;                                   // 打字速度
+var index = 0;                                      // 編號
+var count = 0;                                      // 字數
+var id = "type-box";                                // 要寫入文字的 ID
 
 // 定義方法
 function type(){
     var box = document.getElementById(id);
-    box.innerHTML = texts[index];
+
+    // 變數 切割完的文字 = 文字陣列[編號].切割(開始位置，切幾個);
+    var slice = texts[index].slice(0, count++);
+
+    // 盒子.插入內容 = 切割完的文字;
+    box.innerHTML = slice;
+
+    setTimeout(type,500);
+
+    if (count > texts[index].length+3) {
+        count=0;
+        index++;
+    }
+
+    if (index == texts.length) {
+        index = 0;
+        
+    }
 }
 
 // 當網頁載入完成時執行方法 type
